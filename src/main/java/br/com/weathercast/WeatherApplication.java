fb171df8634982e47a7363bd1383a9215980528d
Sergio Adriano da Siva Filho <sergio-asf@segplan.go.gov.br>
2017-12-20 15:16:28 -0200

    Adicionando react

--- src/main/java/br/com/weathercast/WeatherApplication.java
+++ src/main/java/br/com/weathercast/WeatherApplication.java
@@ -2,11 +2,37 @@ package br.com.weathercast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class WeatherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("OPTIONS");
+	    config.addAllowedMethod("HEAD");
+	    config.addAllowedMethod("GET");
+	    config.addAllowedMethod("PUT");
+	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
+	    config.addAllowedMethod("PATCH");
+	    source.registerCorsConfiguration("/**", config);
+	    // return new CorsFilter(source);
+	    final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+	    bean.setOrder(0);
+	    return bean;
 	}
 }
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+var node_dir = __dirname + '/node_modules';
+
+module.exports = {
+    entry: './src/main/js/index.js',
+    devtool: 'sourcemaps',
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/built/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2017', 'react']
+                }
+
+            }
+        ]
+    }
+};
